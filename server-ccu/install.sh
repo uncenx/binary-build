efa163f0f54393559ecb060f41fdffe9947944f2
zergolf1994 <78160319+zergolf1994@users.noreply.github.com>
2026-03-16 23:00:04 +0700

    Fix : Nginx

--- server-ccu/install.sh
+++ server-ccu/install.sh
@@ -275,6 +275,10 @@ server {
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto \$scheme;
+
+        # WebSocket timeout (keep connection alive, let app handle ping/pong)
+        proxy_read_timeout 86400s;
+        proxy_send_timeout 86400s;
     }
 }
 EOF
